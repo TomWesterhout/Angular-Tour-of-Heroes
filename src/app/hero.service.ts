401f0e64421397ac0635911f4af0c655e8c9141b
TomWesterhout <westerhout_tom@hotmail.com>
2018-05-05 15:09:15 +0200

    Added add() functionality to the heroes component.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -88,7 +88,7 @@ export class HeroService {
   /** PUT: update the hero on the server */
   updateHero (hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
-      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      tap(_ => this.log('updates hero id=${hero.id}')),
       catchError(this.handleError<any>('updateHero'))
     );
   }
